333ec3c0843fac6a8ba5cdfb2e43c10575e9913b
Ash Murphy <amurphy9956@live.com>
2019-11-05 11:43:39 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Package meta-data.
+NAME = 'assault'
+DESCRIPTION = 'A Python based web load testing tool.'
+URL = 'https://github.com/example/assault'
+EMAIL = 'me@example.com'
+AUTHOR = 'Example Person'
+REQUIRES_PYTHON = '>=3.7.0'
+VERSION = '0.1.0'
